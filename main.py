92d3761f59034d88584c1ee75021d9c8cb9dcc38
ryan <github@rgskinner.com>
2020-11-03 20:16:35 -0800

    Demo copy paste

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+successes, failures = pygame.init()
+print("Initializing pygame: {0} successes and {1} failures.".format(successes, failures))
+
+screen = pygame.display.set_mode((720, 480))
+clock = pygame.time.Clock()
+FPS = 60
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((32, 32))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()  # Get rect of some size as 'image'.
+        self.velocity = [0, 0]
+
+    def update(self):
+        self.rect.move_ip(*self.velocity)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+player = Player()
+running = True
+while running:
+    dt = clock.tick(FPS) / 1000  # Returns milliseconds between each call to 'tick'. The convert time to seconds.
+    screen.fill(BLACK)  # Fill the screen with background color.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                player.velocity[1] = -200 * dt  # 200 pixels per second
+            elif event.key == pygame.K_s:
+                player.velocity[1] = 200 * dt
+            elif event.key == pygame.K_a:
+                player.velocity[0] = -200 * dt
+            elif event.key == pygame.K_d:
+                player.velocity[0] = 200 * dt
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_w or event.key == pygame.K_s:
+                player.velocity[1] = 0
+            elif event.key == pygame.K_a or event.key == pygame.K_d:
+                player.velocity[0] = 0
+
+    player.update()
+
+    screen.blit(player.image, player.rect)
+    pygame.display.update()  # Or pygame.display.flip()
+
+print("Exited the game loop. Game will quit...")
+quit()  # Not actually necessary since the script will exit anyway.
